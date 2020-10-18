48cf4b088bc1f46dec5751acdbfaee202eaf43e4
ayush208 <63228097+ayush208@users.noreply.github.com>
2020-10-18 22:53:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+ })
+
+// rotator
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
